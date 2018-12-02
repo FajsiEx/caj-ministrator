f6682da97f38dcdbe8acf4502836d9b916808262
FajsiEx <fajsiexofficial@gmail.com>
2018-12-02 15:20:47 +0100

    added technokitty

--- server.js
+++ server.js
@@ -320,6 +320,120 @@ discordClient.on('ready', ()=>{
                     msg.channel.send("Rest in piss, forever miss...");
                     break;
 
+                case "technokitty":
+                    msg.channel.send({
+                        "embed": {
+                            "title": "Techno kitty by *S3RL*",
+                            "color": BLUE,
+                            "description": `
+                                Up in the sky
+                                Flyiiing way up high
+                                I can't beliieve my eyes
+                                A techno feeeline
+                                so fly
+                                Up high
+                                And neeeveer come down
+                                I'll move
+                                Like you
+                                To the techno sound
+
+                                ***TECHNO CAT***
+
+                                Techno kitty, kitty
+                                You're so pretty kitty
+                                Techno Kitty with a rocket pack
+
+                                Techno kitty, kitty
+                                You're so pretty kitty
+                                Techno Kitty with wings on your back
+
+                                Flying through the air so high
+                                You're like a shooting star that lights up the sky
+
+                                Techno kitty, kitty
+                                You're so pretty kitty
+                                Techno kitty
+                                Tec-tec-techno cat
+
+                                ***TECHNO CAT***
+                                ***TECHNO CAT***
+
+                                Techno kitty, kitty
+                                You're so pretty kitty
+                                Techno Kitty with a rocket pack
+
+                                Techno kitty, kitty
+                                You're so pretty kitty
+                                Techno Kitty with wings on your back
+
+                                Flying through the air so high
+                                You're like a shooting star that lights up the sky
+
+                                Techno kitty, kitty
+                                You're so pretty kitty
+                                Techno kitty
+                                Tec-tec-techno cat
+
+                                How?
+
+                                *Hooow?*
+
+                                How'd you get there
+                                Way up in the air
+                                You're souring so free
+                                Cute fluffy kitty
+
+                                So fly
+                                Up high
+                                And neeeveer come down
+                                I'll move
+                                Like you
+                                To the techno sound
+
+                                ***TECHNO CAT***
+
+                                Techno kitty, kitty
+                                You're so pretty kitty
+                                Techno Kitty with a rocket pack
+
+                                Techno kitty, kitty
+                                You're so pretty kitty
+                                Techno Kitty with wings on your back
+
+                                Flying through the air so high
+                                You're like a shooting star that lights up the sky
+
+                                Techno kitty, kitty
+                                You're so pretty kitty
+                                Techno kitty
+                                Tec-tec-techno cat
+
+                                ***TECHNO CAT***
+
+                                Techno kitty, kitty
+                                You're so pretty kitty
+                                Techno Kitty with a rocket pack
+
+                                Techno kitty, kitty
+                                You're so pretty kitty
+                                Techno Kitty with wings on your back
+
+                                Flying through the air so high
+                                You're like a shooting star that lights up the sky
+
+                                Techno kitty, kitty
+                                You're so pretty kitty
+                                Techno kitty
+                                Tec-tec-techno cat
+
+                                How?
+
+                                *Hooow?*
+                            `
+                        }
+                    });
+                    break;
+
                 case "meme":
                 case "meirl":
                     request({
