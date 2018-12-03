7f14b6aa3d5d3b8d49ef30f4cc18ef00bda08766
FajsiEx <fajsiexofficial@gmail.com>
2018-12-03 19:07:59 +0100

    msgeeggreply is now case-insens. & added no u reply

--- modules/jffModule.js
+++ modules/jffModule.js
@@ -1,10 +1,21 @@
+/*
+
+    This module adds various easter eggs and shit like that
+    Nothing special
+
+*/
+
 
 module.exports = {
     msgEaterEggReply: (msg, message)=>{
-        if (message.indexOf('click the circles') > -1) {
+        message = message.toLocaleLowerCase(); //
+        if (message.to.indexOf('click the circles') > -1) {
             msg.reply(`to the beat. ***CIRCLES!***`);
             return;
         }else if (message.indexOf('fuck you') > -1) {
+            msg.reply(`no u`);
+            return;
+        }else if (message.indexOf('no u') > -1) {
             msg.reply(`no u`);
             return;
         }else if (message.indexOf('e621') > -1) {
