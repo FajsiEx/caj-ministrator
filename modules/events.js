725f0b8990c56b787e756d6dbed48391fc7a12dc
FajsiEx <fajsiexofficial@gmail.com>
2018-12-15 16:31:11 +0100

    test

--- modules/events.js
+++ modules/events.js
@@ -94,8 +94,6 @@ module.exports = {
         // events = events.filter((e)=>{
         //     return e.time < new Date().getTime();
         // });
-
-        globalVariables.set("events", events);
     
         let todayDateString = `${new Date().getDate()}.${new Date().getMonth()+1}.${new Date().getFullYear()}`;
         
