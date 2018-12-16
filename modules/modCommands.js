419604ff73728b226822703f4bf7578d71a8136f
FajsiEx <fajsiexofficial@gmail.com>
2018-12-16 13:45:14 +0100

    moved nuke to its own module

---
+++ modules/modCommands.js
@@ -0,0 +1,46 @@
+
+module.exports = {
+    nuke: (msg, commandMessageArray)=>{
+        if (commandMessageArray[1].normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLocaleLowerCase().indexOf("me") > -1) {
+            msg.channel.send({
+                "embed": {
+                    "title": "Hey,",
+                    "description": "how 'bout you fuck off. Seriously. Don't. This is not place for this. Stop it, get some help.",
+                    "color": COLORS.RED
+                }
+            }).then(msg => msg.delete(10000));
+            return;
+        }
+    
+        if(!smallFunctions.checkAdmin(msg)) {
+            msg.channel.send({
+                "embed": {
+                    "title": "Tento príkaz môžu vykonávať len admini lol",
+                    "color": COLORS.RED
+                }
+            });
+            return;
+        }
+    
+        let limit = parseInt(commandMessageArray[1]);
+        if(!limit) {
+            msg.channel.send({
+                "embed": {
+                    "title": "Chýba koľko správ vymazať",
+                    "color": COLORS.RED
+                }
+            });
+            return;
+        }
+    
+        console.log("[NUKE] Nuked "+ limit + " messages.");
+        msg.channel.bulkDelete(limit).then(() => {
+            msg.channel.send({
+                "embed": {
+                    "title": "Vymazal som "+ limit + " správ.",
+                    "color": COLORS.GREEN
+                }
+            }).then(msg => msg.delete(5000));
+        });
+    }
+}
--- modules/msgHandler.js
+++ modules/msgHandler.js
@@ -10,6 +10,7 @@ const globalVariables = require("./globalVariables");
 const eventsModule = require("./events");
 const smallFunctions = require("./smallFunctions");
 const infoCommands = require("./infoCommands");
+const modCommands = require("./modCommands");
 
 const RESTRICTED_MODE = require("./consts").RESTRICTED_MODE;
 const TEST_CHANNEL_ID = require("./consts").TEST_CHANNEL_ID;
@@ -579,48 +580,7 @@ module.exports = (msg, discordClient)=>{
                 }
 
             case "nuke":
-                if (commandMessageArray[1].normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLocaleLowerCase().indexOf("me") > -1) {
-                    msg.channel.send({
-                        "embed": {
-                            "title": "Hey,",
-                            "description": "how 'bout you fuck off. Seriously. Don't. This is not place for this. Stop it, get some help.",
-                            "color": COLORS.RED
-                        }
-                    }).then(msg => msg.delete(10000));
-                    return;
-                }
-
-                if(!smallFunctions.checkAdmin(msg)) {
-                    msg.channel.send({
-                        "embed": {
-                            "title": "Tento príkaz môžu vykonávať len admini lol",
-                            "color": COLORS.RED
-                        }
-                    });
-                    return;
-                }
-
-                let limit = parseInt(commandMessageArray[1]);
-                if(!limit) {
-                    msg.channel.send({
-                        "embed": {
-                            "title": "Chýba koľko správ vymazať",
-                            "color": COLORS.RED
-                        }
-                    });
-                    return;
-                }
-
-                console.log("[NUKE] Nuked "+ limit + " messages.");
-                msg.channel.bulkDelete(limit).then(() => {
-                    msg.channel.send({
-                        "embed": {
-                            "title": "Vymazal som "+ limit + " správ.",
-                            "color": COLORS.GREEN
-                        }
-                    }).then(msg => msg.delete(5000));
-                });
-
+                modCommands.nuke(msg, commandMessageArray);
                 break;
 
             case "nick":
