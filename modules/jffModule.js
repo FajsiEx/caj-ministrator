515c1eed4a613265c11a144a764d2567bed0a9f7
FajsiEx <fajsiexofficial@gmail.com>
2018-12-19 21:19:40 +0100

    move / roll from msgHandler to jffModule

--- modules/jffModule.js
+++ modules/jffModule.js
@@ -177,6 +177,73 @@ module.exports = {
         }
     },
 
+    roll: (msg, commandMessageArray)=>{
+        let min,max;
+        if (commandMessageArray[2]) {
+            min = parseInt(commandMessageArray[1]);
+            max = parseInt(commandMessageArray[2]);
+        }else{
+            max = parseInt(commandMessageArray[1]);
+        }
+        
+
+        if(!max) {
+            max = 100;
+        }
+        if(min > max) {
+            msg.channel.send({
+                "embed": {
+                    "title": "Nesprávne použitie príkazu",
+                    "color": COLORS.RED,
+                    "description": "!roll\n!roll [max]\n!roll [min] [max]",
+                }
+            });
+            return;
+        }
+
+        if (max == 621) {
+            msg.channel.send({ // TODO: Move this to consts module
+                "embed": {
+                    "title": "Error",
+                    "color": COLORS.RED,
+                    "description": "Pri vykonávaní tohto príkazu nastala nečakaná chyba. Fuck.",
+                    "fields": [
+                        {
+                            "name": "Error details:",
+                            "value": `
+                                msgHandler.js:310
+                                let rolled = Math.floor(Math.random() * (max + 1))
+                                             ^
+                            
+                                Error: I. Refuse. I'm done with humanity. Period. I don't know why you did it, but no. I won't do this. Please save me.
+                                    at Math.floor (server.js:308:13)
+                                    at discordClient.on (server.js:147:0)
+                                    at discordClient.on (server.js:127:0)
+                                    at node.js:0:0
+                            `
+                        }
+                    ]
+                }
+            });
+            return;
+        }
+
+        let rolled;
+        if (min) {
+            rolled = Math.floor((min) + Math.random() * (max - min + 1));
+        }else{
+            rolled = Math.floor(Math.random() * (max + 1));
+        }
+
+        let quest = msg.content.slice(6);
+
+        if (quest == "") {
+            quest = "hodil"
+        }
+
+        msg.reply(quest + ": **" + rolled + "**");
+    },
+
     spravnyPrikazCommand: (msg)=>{
         msg.reply({
             "embed": {
--- modules/msgHandler.js
+++ modules/msgHandler.js
@@ -313,76 +313,7 @@ module.exports = (msg, discordClient)=>{
                 break;
 
             case "roll":
-                let min,max;
-                if (commandMessageArray[2]) {
-                    min = parseInt(commandMessageArray[1]);
-                    max = parseInt(commandMessageArray[2]);
-                }else{
-                    max = parseInt(commandMessageArray[1]);
-                }
-                
-
-                if(!max) {
-                    max = 100;
-                }
-                if(min > max) {
-                    msg.channel.send({
-                        "embed": {
-                            "title": "Nesprávne použitie príkazu",
-                            "color": COLORS.RED,
-                            "description": "!roll\n!roll [max]\n!roll [min] [max]",
-                        }
-                    });
-                    return;
-                }
-
-                if (max == 621) {
-                    msg.channel.send({
-                        "embed": {
-                            "title": "Error",
-                            "color": COLORS.RED,
-                            "description": "Pri vykonávaní tohto príkazu nastala nečakaná chyba. Fuck.",
-                            "fields": [
-                                {
-                                    "name": "Error details:",
-                                    "value": `
-                                        msgHandler.js:310
-                                        let rolled = Math.floor(Math.random() * (max + 1))
-                                                     ^
-                                    
-                                        Error: I. Refuse. I'm done with humanity. Period. I don't know why you did it, but no. I won't do this. Please save me.
-                                            at Math.floor (server.js:308:13)
-                                            at discordClient.on (server.js:147:0)
-                                            at discordClient.on (server.js:127:0)
-                                            at node.js:0:0
-                                    `
-                                }
-                            ]
-                        }
-                    });
-                    return;
-                }
-
-                let rolled;
-                if (min) {
-                    rolled = Math.floor((min) + Math.random() * (max - min + 1));
-                }else{
-                    rolled = Math.floor(Math.random() * (max + 1));
-                }
-
-                let quest = msg.content.slice(6);
-
-                if (quest == "") {
-                    quest = "hodil"
-                }else{
-                    switch(quest) {
-                        case "sanca ze pojdes do pekla":
-                            rolled = 100;
-                            break;
-                    }
-                }
-
-                msg.reply(quest + ": **" + rolled + "**");
+                jffModule.roll(msg, commandMessageArray);
                 break;
 
             case "ahoj": //robil Dan Valnicek
