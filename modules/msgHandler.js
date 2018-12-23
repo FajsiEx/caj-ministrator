b98878916c1d00a8b5b45b83fc354967c13ea0e8
FajsiEx <fajsiexofficial@gmail.com>
2018-12-23 17:33:42 +0100

    bugfix / FINAL FIX YES

--- modules/msgHandler.js
+++ modules/msgHandler.js
@@ -298,13 +298,16 @@ module.exports = (msg, discordClient)=>{
                     }
                 }).then((sentMsg)=>{
                     let request = e621.random("m/m", "E", 1, post => {
-                        sentMsg.delete();
+                        
                         msg.channel.send({
                             "embed": {
-                                "title": "",
+                                "title": "by " + post[0]['author'],
+                                "description": "**Favs:** " + post[0].fav_count + "\n**Artists:** " + JSON.stringify(post[0].artist) + "\n**Tags:** " + JSON.stringify(post[0].tags),
                                 "color": COLORS.GREEN
                             },
                             "files": [post[0]['file_url']]
+                        }).then(()=>{
+                            sentMsg.delete();
                         });
                         console.log("[DEBUG] E621:" + JSON.stringify(post));
                     });
