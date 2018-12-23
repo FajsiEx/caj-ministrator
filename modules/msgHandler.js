7bd2e833711641edb46bc84876b03ffe78db9382
FajsiEx <fajsiexofficial@gmail.com>
2018-12-23 15:46:09 +0100

    add / unmute command & self-destruct of err msgs in mute unmute

--- modules/msgHandler.js
+++ modules/msgHandler.js
@@ -535,9 +535,50 @@ module.exports = (msg, discordClient)=>{
                             "title": "Tento príkaz môžu vykonávať len admini lol",
                             "color": COLORS.RED
                         }
-                    });
+                    }).then(msg => msg.delete(5000));
                     return;
                 }
+                break;
+
+            case "un":
+            case "unsilence":
+                if(smallFunctions.checkAdmin(msg)) {
+                    let mentionList = msg.mentions.users;
+                    console.log("[DEBUG] Unsilence, ML(" + JSON.stringify(mentionList))
+                    if(mentionList.array().length == 0) {
+                        msg.channel.send({
+                            "embed": {
+                                "title": "Boi tomu nechápem. Šak !unmute/unsilence @niekto [Nemám koho unmutnút]",
+                                "color": COLORS.RED
+                            }
+                        }).then(msg => msg.delete(10000));
+                        return;
+                    }
+
+                    let role = msg.guild.roles.find(r => r.name == "Muted");
+                    let user = msg.mentions.members.first();
+
+                    console.log("[MUTE] Unmuted "+ user.name + ".");
+                    user.removeRole(role).catch(console.error);
+
+                    console.log("[MUTE] Unmuted "+ user.username + "#" + user.discriminator);
+                    msg.channel.send({
+                        "embed": {
+                            "title": user.username + "#" + user.discriminator + " bol unmutnutý.",
+                            "color": COLORS.GREEN
+                        }
+                    }).then(msg => msg.delete(10000));
+                    return;
+                }else{
+                    msg.channel.send({
+                        "embed": {
+                            "title": "Tento príkaz môžu vykonávať len admini lol",
+                            "color": COLORS.RED
+                        }
+                    }).then(msg => msg.delete(5000));
+                    return;
+                }
+                break;
 
             case "nuke":
                 modCommands.nuke(msg, commandMessageArray);
