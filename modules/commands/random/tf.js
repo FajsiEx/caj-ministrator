7e5a6c19b9e0590c23635b018e301dc3a0bacaef
FajsiEx <fajsiexofficial@gmail.com>
2018-12-28 14:31:32 +0100

    cleanup / removed unused quest var in tf module

--- modules/commands/random/tf.js
+++ modules/commands/random/tf.js
@@ -3,8 +3,6 @@ module.exports = {
     command: function(msg) {
         let rolled = Math.floor(Math.random() * 100);
 
-        let quest = msg.content.slice(4);
-
         if (rolled <= 50) {
             rolled = "nie";
         }else{
