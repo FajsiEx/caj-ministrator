f2c036d1a3fe157689d88c5913b53a805d88b820
FajsiEx <fajsiexofficial@gmail.com>
2019-01-16 18:09:31 +0100

    update / new versioning and actually fixed the 404 url

--- modules/commands/info/help.js
+++ modules/commands/info/help.js
@@ -8,8 +8,8 @@ module.exports = {
                 "title": "Help",
                 "color": COLORS.BLUE,
                 "description": `
-                    Tea-bot project 2019.1.1
-                    [Všetky príkazy](https://fajsiex.ml/docs/caj-bot.html)
+                    Tea-bot project 2019.1.2
+                    [Všetky príkazy](https://fajsiex.ml/docs/tea-bot.html)
                     [GitHub repo (source)](https://github.com/FajsiEx/tea-bot)
                 `
             }
