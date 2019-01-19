a6abdf2711b91263bccf3fb924181d7a5abe32bc
FajsiEx <fajsiexofficial@gmail.com>
2019-01-19 12:34:46 +0100

    cleanup / removed unused import in nick.js

--- modules/commands/jff/nick.js
+++ modules/commands/jff/nick.js
@@ -1,6 +1,4 @@
 
-const DEV_USERID = require("../../consts").DEV_USERID;
-
 module.exports = {
     command: function(msg, discordClient) {
         msg.guild.member(discordClient.user).setNickname(msg.content.slice(6)); // !nick bla => bla
