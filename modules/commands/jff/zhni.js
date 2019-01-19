4730ca2dba0f5612783cbc1fca4729f01b33dbea
CodyM4V3R1CK <bobkomatej1@gmail.com>
2019-01-19 21:23:50 +0100

    zhni.js fix of multiple sentences-back to normal

--- modules/commands/jff/zhni.js
+++ modules/commands/jff/zhni.js
@@ -1,8 +1,6 @@
 
 module.exports = {
     command: function(msg) {
-        msg.channel.send(
-            "Sa najedz ked si zhni lol",
-            "Ked si zhni, nie si to ty");
+        msg.channel.send( "Sa najedz ked si zhni lol",);
     }
 }
