52bf2f104de71039d4e932457fb3601671b69fd5
CodyM4V3R1CK <bobkomatej1@gmail.com>
2019-01-19 23:37:30 +0100

    update / version change

--- modules/consts.js
+++ modules/consts.js
@@ -5,7 +5,7 @@
 */
 
 module.exports = {
-    VERSION: "2019.1.6",
+    VERSION: "2019.1.7",
 
     WEEK_DAYS: ["Nedeľa", "Pondelok", "Utorok", "Streda", "Štvrtok", "Piatok", "Sobota"],
     WEEK_DAYS_SHORT: ["Ne", "Po", "Ut", "St", "Št", "Pi", "So"],
