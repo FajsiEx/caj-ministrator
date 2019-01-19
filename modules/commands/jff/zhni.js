3ad4bc83cf5a00266e8b00eabc8202b1461a669c
FajsiEx <fajsiexofficial@gmail.com>
2019-01-19 22:18:18 +0100

    add / more replies to zhni command

--- modules/commands/jff/zhni.js
+++ modules/commands/jff/zhni.js
@@ -1,6 +1,10 @@
 
 module.exports = {
     command: function(msg) {
-        msg.channel.send( "Sa najedz ked si zhni lol");
+        const ZHNI_REPLIES = [
+            "Sa najedz ked si zhni lol",
+            "Keď si zhni, nie si to ty"
+        ]
+        msg.channel.send(ZHNI_REPLIES[Math.floor(Math.random() * ZHNI_REPLIES.length)]);
     }
 }
