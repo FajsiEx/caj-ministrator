8796ae8cfc9a05c0591e31f00f109a1ae05709d8
CodyM4V3R1CK <bobkomatej1@gmail.com>
2019-01-20 10:10:10 +0100

    add / command what have I done

--- modules/botCommands.js
+++ modules/botCommands.js
@@ -243,6 +243,11 @@ let commands = {
 
     'wwtf': (msg)=>{meme.command(msg, "wwtf")},
 
+    'whid': (msg)=>{meme.command(msg, "whid")},
+    'whId': (msg)=>{meme.command(msg, "whid")},
+    'whathaveidone': (msg)=>{meme.command(msg, "whid")},
+    'whathaveIdone': (msg)=>{meme.command(msg, "whid")},
+
     'gj': (msg)=>{meme.command(msg, "gj")},
     'greatjob': (msg)=>{meme.command(msg, "gj")},
 
--- modules/commands/jff/meme.js
+++ modules/commands/jff/meme.js
@@ -24,7 +24,8 @@ const memeUrls = {
     way: "https://i.imgflip.com/234zr7.jpg",
     monkas: "https://cdn.vox-cdn.com/thumbor/ITErCh1_JR7_GwWdMVmM9WRFwu4=/0x0:1200x675/1200x0/filters:focal(0x0:1200x675):no_upscale()/cdn.vox-cdn.com/uploads/chorus_asset/file/10838143/monkas.png",
     gj: "https://i.redd.it/yqzqyob3lnv01.jpg",
-    booty: "https://i.kym-cdn.com/photos/images/original/000/811/629/bf6.gif"
+    booty: "https://i.kym-cdn.com/photos/images/original/000/811/629/bf6.gif",
+    whid: "http://www.reactiongifs.com/r/whid1.gif"
 }
 
 module.exports = {
