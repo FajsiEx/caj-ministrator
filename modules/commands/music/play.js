98316007ac687eb7ba053c07e21eb2ea5ae4e47c
FajsiEx <fajsiexofficial@gmail.com>
2019-01-22 19:19:50 +0100

    update / basic music play comm now works

--- modules/commands/music/play.js
+++ modules/commands/music/play.js
@@ -1,17 +1,10 @@
 
 const COLORS = require("../../consts").COLORS;
+const ytdl = require("ytdl-core");
 
 module.exports = {
     command: function(msg) {
-        msg.channel.send({
-            "embed": {
-                "title": "Play",
-                "color": COLORS.BLUE,
-                "description": `
-                    Debug test
-                `
-            }
-        });
+        let commandMessageArray = msg.content.split(" ");
 
         let vc = msg.member.voiceChannel;
         if (!vc) {
@@ -26,6 +19,60 @@ module.exports = {
             });
             return;
         }
-        vc.join();
+
+        let song = commandMessageArray[1]
+        if (!song) {
+            msg.channel.send({
+                "embed": {
+                    "title": "Play",
+                    "color": COLORS.RED,
+                    "description": `
+                        Neni link ;(
+                    `
+                }
+            });
+            return;
+        }
+
+
+        let isValidYTlink = song.startsWith("https://www.youtube.com/watch?v=") || song.startsWith("https://youtube.com/watch?v=")
+
+        if (!isValidYTlink) {
+            msg.channel.send({
+                "embed": {
+                    "title": "Play",
+                    "color": COLORS.GREEN,
+                    "description": `
+                        Prijmam len YT video linky (zatiaľ)
+                    `
+                }
+            });
+            return;
+        }
+
+        vc.join().then((connection)=>{
+            let audioStream = ytdl(
+                song, {
+                audioonly: true
+            });
+    
+            connection.playStream(audioStream);
+
+            msg.channel.send({
+                "embed": {
+                    "title": "Play",
+                    "color": COLORS.RED,
+                    "description": `
+                        Hrajem ${song}.
+                    `
+                }
+            });
+
+            msg.delete();
+        });
+
+        
+
+
     }
 }
